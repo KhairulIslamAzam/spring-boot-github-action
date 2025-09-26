e7c71db75f64d47c91f6009df78e0093ef4c99bf
khairul <khairul.islam@brainstation-23.com>
2025-09-26 23:53:23 +0600

    test config

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloControllerTest {
-   private HelloService helloService;
+
     @Test
     void testHelloEndpoint() {
+        HelloService helloService = new HelloService();
         HelloController controller = new HelloController(helloService);
         assertThat(controller).isNotNull();
     }
